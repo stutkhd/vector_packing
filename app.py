1d75fe2413abb78fc4d09048725d4068287f5ccf
Takahide Sato <whydoyoustudy@gmail.com>
2021-12-03 00:15:52 +0900

    [add] problem explanation

--- app.py
+++ app.py
@@ -5,40 +5,13 @@ import pandas as pd
 
 st.title('d次元ベクトルパッキング')
 
-def vbpp(s,B,c,penalty=1000.):
-    n = len(s)
-    U = len(c)
-    model = Model("bpp")
-    # setParam("MIPFocus",1)
-    x, y, z = {},{},{}
-    for i in range(n):
-        z[i] = model.addVar(vtype="B", name= f"z({i})")
-        for j in range(U):
-            x[i,j] = model.addVar(vtype="B", name= f"x({i},{j})")
-    for j in range(U):
-        y[j] = model.addVar(vtype="B", name= f"y({j})")    
-    model.update()
-    # assignment constraints
-    for i in range(n):
-        model.addConstr(quicksum(x[i,j] for j in range(U)) + z[i] == 1, f"Assign({i})")
-    # tighten assignment constraints
-    for j in range(U):
-        for i in range(n):
-            model.addConstr(x[i,j] <= y[j], f"Strong({i},{j})")
-    # bin capacity constraints
-    for j in range(U):
-        for k in range(d):
-            model.addConstr(quicksum(s[i,k]*x[i,j] for i in range(n)) <= B[j,k]*y[j], f"Capac({j},{k})")
-    model.setObjective(quicksum(penalty*s[i,k]*z[i] for i in range(n) for k in range(d)) + quicksum(c[j]*y[j] for j in range(U)), GRB.MINIMIZE)
+st.header('ビンパッキング問題とは')
+st.latex(r'''
+          容量c(>0)の箱とn個の荷物N={1,…,n}が与えられている。\\
+          荷物i∈Nの容量をw_i(>0)とする。\\
+          全ての荷物を詰合わせるのに必要な箱の個数を最小にする詰合わせを求めよ。 \\
+         ''')
 
-    model.update()
-    model.__data = x,y,z
-    return model
-
-"""
-w: アイテムの集合
-B: ビンの集合
-"""
 def BDF(w, B, alpha=1.):
     """
     Item Based Best Fit Decreasing (BFD) heuristcs for variable size vector packing problem
@@ -46,7 +19,7 @@ def BDF(w, B, alpha=1.):
     # n, d = w.shape # n: 個数, d: 次元
     U = len(B) # 瓶の個数
     unassigned = []
-    bins = [[] for j in range(U)]# ビンの容量
+    bins = [[] for j in range(U)] # ビンの容量
     while len(w)>0:
         W = w.sum(axis=0) #アイテムに対する残りサイズの和（次元ごと）
         R = B.sum(axis=0) #ビンに対する残り容量の和（次元ごと）
@@ -94,8 +67,6 @@ def create_1D_graph(bins):
   )
   st.bar_chart(chart_data)
 
-select_method = st.sidebar.selectbox('アルゴリズム', ['BDF','Normal'])
-
 np.random.seed(123)
 n = st.sidebar.number_input(label='アイテム個数',min_value=1, value=5)
 U = st.sidebar.number_input(label='ビン数', min_value=1, value=3)
@@ -109,9 +80,6 @@ with st.expander("ビンの容量制限"):
   blb = st.number_input(label='容量最小値',min_value=1, value=15)
   bub = st.number_input(label='容量最大値',min_value=int(blb)+1, value=20)
 
-if (select_method == 'Normal'):
-  penalty = st.sidebar.number_input(label='ペナルティ', value=1000)
-
 w = np.random.randint(lb,ub,(n,d)) # アイテム配列
 B = np.random.randint(blb,bub, (U,d)) # ビン配列
 
@@ -120,20 +88,8 @@ st.text(w)
 st.write('ビン集合')
 st.text(B)
 
-if select_method == 'Normal':
-  c = np.random.randint(500, 1000, U)
-  model = vbpp(w, B, c, penalty)
-  model.optimize()
-  x,y,z = model.__data
-  bins = [[] for j in range(U)]
-  unassigned =[]
-  for i in z:
-      if z[i].X > .5:
-          unassigned.append(w[i])
-  # bins, unassigned = 
-  pass
-elif select_method == 'BDF':
-  bins, unassigned = BDF(w, B, alpha=1.)
+bins, unassigned = BDF(w, B, alpha=1.)
+
 st.write('ビン')
 st.text(bins)
 st.write('残りアイテム')
@@ -141,5 +97,5 @@ st.text(unassigned)
 st.write('残ビン容量')
 st.text(B)
 
-if select_method == 'BDF' and d == 1:
+if d == 1:
   create_1D_graph(bins)
